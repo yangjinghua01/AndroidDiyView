9d735e934bba74b1c6be5430fa4759adec12dda1
16920 <1692047690@qq.com>
2022-08-31 22:54:32 +0800

    2022年8月31日 22:54:16 定义外圆弧

--- app/src/main/java/com/rgsc/myapplication/mydiyText.java
+++ app/src/main/java/com/rgsc/myapplication/mydiyText.java
@@ -1,35 +1,24 @@
-package com.rgsc.myapplication;
-
-import android.content.Context;
-import android.content.res.TypedArray;
-import android.graphics.Canvas;
-import android.graphics.Color;
-import android.graphics.Paint;
-import android.graphics.Rect;
-import android.util.AttributeSet;
-import android.view.MotionEvent;
-import android.view.View;
-
-import androidx.annotation.Nullable;
-
-public class mydiyText extends View {
-    private String text;
-    private int diyColor = Color.BLACK;
-    private Paint mPain;
-
-    public mydiyText(Context context) {
-        super(context);
-    }
-
-    public mydiyText(Context context, @Nullable AttributeSet attrs) {
-        super(context, attrs);
-    }
-
-    public mydiyText(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
-        super(context, attrs, defStyleAttr);
-    }
-
-
+//package com.rgsc.myapplication;
+//
+//import android.content.Context;
+//import android.content.res.TypedArray;
+//import android.graphics.Canvas;
+//import android.graphics.Color;
+//import android.graphics.Paint;
+//import android.graphics.Rect;
+//import android.util.AttributeSet;
+//import android.view.MotionEvent;
+//import android.view.View;
+//
+//import androidx.annotation.Nullable;
+//
+//public class mydiyText extends View {
+//    private String text;
+//    private int diyColor = Color.BLACK;
+//    private Paint mPain;
+//
+//
+//
 //    public mydiyText(Context context) {
 //        this(context,null);
 //    }
@@ -49,40 +38,40 @@ public class mydiyText extends View {
 //        mPain.setColor(diyColor);
 //        mPain.setTextSize(1f);
 //    }
-
-    @Override
-    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
-        int WidthMode = MeasureSpec.getMode(widthMeasureSpec);
-        int HeightMode = MeasureSpec.getMode(heightMeasureSpec);
-        int width = MeasureSpec.getSize(widthMeasureSpec);
-        int heigth = MeasureSpec.getSize(heightMeasureSpec);
-        if (WidthMode == MeasureSpec.AT_MOST){
-            Rect rect = new Rect();
-            mPain.getTextBounds(text,0,text.length(),rect);
-             width = rect.width();
-        }
-        if (HeightMode == MeasureSpec.AT_MOST){
-            Rect rect = new Rect();
-            mPain.getTextBounds(text,0,text.length(),rect);
-            heigth = rect.height();
-        }
-        setMeasuredDimension(width,heigth);
-
-    }
-
-    @Override
-    protected void onDraw(Canvas canvas) {
-        super.onDraw(canvas);
-        Paint.FontMetricsInt fontMetricsInt = mPain.getFontMetricsInt();
-        int dy = (fontMetricsInt.bottom - fontMetricsInt.top)/2-fontMetricsInt.bottom;
-        int baseline = getHeight()/2+dy;
-        int x = getPaddingLeft();
-        canvas.drawText(text,x,baseline,mPain);
-    }
-
-    @Override
-    public boolean onTouchEvent(MotionEvent event) {
-        return super.onTouchEvent(event);
-    }
-}
+//
+//    @Override
+//    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+//        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+//        int WidthMode = MeasureSpec.getMode(widthMeasureSpec);
+//        int HeightMode = MeasureSpec.getMode(heightMeasureSpec);
+//        int width = MeasureSpec.getSize(widthMeasureSpec);
+//        int heigth = MeasureSpec.getSize(heightMeasureSpec);
+//        if (WidthMode == MeasureSpec.AT_MOST){
+//            Rect rect = new Rect();
+//            mPain.getTextBounds(text,0,text.length(),rect);
+//             width = rect.width();
+//        }
+//        if (HeightMode == MeasureSpec.AT_MOST){
+//            Rect rect = new Rect();
+//            mPain.getTextBounds(text,0,text.length(),rect);
+//            heigth = rect.height();
+//        }
+//        setMeasuredDimension(width,heigth);
+//
+//    }
+//
+//    @Override
+//    protected void onDraw(Canvas canvas) {
+//        super.onDraw(canvas);
+//        Paint.FontMetricsInt fontMetricsInt = mPain.getFontMetricsInt();
+//        int dy = (fontMetricsInt.bottom - fontMetricsInt.top)/2-fontMetricsInt.bottom;
+//        int baseline = getHeight()/2+dy;
+//        int x = getPaddingLeft();
+//        canvas.drawText(text,x,baseline,mPain);
+//    }
+//
+//    @Override
+//    public boolean onTouchEvent(MotionEvent event) {
+//        return super.onTouchEvent(event);
+//    }
+//}
