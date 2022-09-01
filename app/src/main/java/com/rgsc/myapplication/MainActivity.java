fec22b6997ce0d004659468a8afcdd4e4e7f8cff
16920 <1692047690@qq.com>
2022-09-01 23:20:06 +0800

    2022年9月1日 23:19:24 qq运动仿基本样子提交睡觉。写代码使我快乐

--- app/src/main/java/com/rgsc/myapplication/MainActivity.java
+++ app/src/main/java/com/rgsc/myapplication/MainActivity.java
@@ -2,7 +2,10 @@ package com.rgsc.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.animation.ObjectAnimator;
+import android.animation.ValueAnimator;
 import android.os.Bundle;
+import android.view.animation.DecelerateInterpolator;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        QQStepView qqStepView = findViewById(R.id.stepview);
+        qqStepView.setmStepMax(4000);
+//        属性动画
+        ValueAnimator valueAnimator = ObjectAnimator.ofFloat(0, 2000);
+        valueAnimator.setDuration(1500);
+        valueAnimator.setInterpolator(new DecelerateInterpolator());
+        valueAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                float animatedValue = (float) animation.getAnimatedValue();
+                qqStepView.setmCurrentStep((int) animatedValue);
+            }
+        });
+        valueAnimator.start();
     }
 }
