786a7f59902a828533a929c8dc9d2387d9db0d39
16920 <1692047690@qq.com>
2022-09-29 21:41:56 +0800

    2022年9月29日 21:40:55 提交代码类似于popwindow带动画效果的筛选菜单

--- app/src/main/java/com/rgsc/myapplication/activity/WuBaActivity.java
+++ app/src/main/java/com/rgsc/myapplication/activity/WuBaActivity.java
@@ -5,12 +5,17 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.rgsc.myapplication.R;
+import com.rgsc.myapplication.adapter.ListScreenMenuAdapter;
+import com.rgsc.myapplication.outer.ListDataScreenView;
+
 
 public class WuBaActivity extends AppCompatActivity {
-
+private ListDataScreenView listDataScreenView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_wu_ba);
+        listDataScreenView = findViewById(R.id.list_data_screen_view);
+        listDataScreenView.setAdapter( new ListScreenMenuAdapter(WuBaActivity.this));
     }
 }
--- app/src/main/java/com/rgsc/myapplication/widgetViewGroup/ListDataScreenView.java
+++ app/src/main/java/com/rgsc/myapplication/widgetViewGroup/ListDataScreenView.java
@@ -10,6 +10,8 @@ import android.widget.LinearLayout;
 
 import androidx.annotation.Nullable;
 
+import com.rgsc.myapplication.adapter.BaseMenuAdapter;
+
 public class ListDataScreenView extends LinearLayout {
     //用来创建头部用来存放Tab
     private LinearLayout mMenuTabView;
@@ -23,6 +25,7 @@ public class ListDataScreenView extends LinearLayout {
     private int mShadowColor = Color.parseColor("#999999");
     //    用来存放菜单内容
     private FrameLayout mMenuContainerView;
+    private BaseMenuAdapter menuAdapter;
 
     public ListDataScreenView(Context context) {
         this(context, null);
@@ -42,6 +45,7 @@ public class ListDataScreenView extends LinearLayout {
      * 实例化好布局
      */
     private void initLayout() {
+        setOrientation(VERTICAL);
 //        1创建头部
         mMenuTabView = new LinearLayout(context);
         mMenuTabView.setLayoutParams(new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT));
@@ -49,8 +53,8 @@ public class ListDataScreenView extends LinearLayout {
 //        创建frameLayout 用来存放阴影（view）+菜单布局
         mMenuMiddleView = new FrameLayout(context);
         LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, 0);
-        layoutParams.weight =1;
-        mMenuTabView.setLayoutParams(layoutParams);
+        layoutParams.weight = 1;
+        mMenuMiddleView.setLayoutParams(layoutParams);
         addView(mMenuMiddleView);
 //        创建阴影不用设置LayoutParams
         mShadowView = new View(context);
@@ -61,5 +65,41 @@ public class ListDataScreenView extends LinearLayout {
         mMenuContainerView.setBackgroundColor(Color.GRAY);
         mMenuMiddleView.addView(mMenuContainerView);
 
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        int height = MeasureSpec.getSize(heightMeasureSpec);
+        int mmenuContainerHeight = (int) (height*0.75/100);
+        ViewGroup.LayoutParams layoutParams = mMenuContainerView.getLayoutParams();
+        layoutParams.height = mmenuContainerHeight;
+        mMenuContainerView.setLayoutParams(layoutParams);
+    }
+
+    /**
+     * 设置Adapter
+     *
+     * @param menuAdapter
+     */
+    public void setMenuAdapter(BaseMenuAdapter menuAdapter) {
+        if (menuAdapter == null) {
+            throw new NullPointerException("设配器为空");
+        }
+        this.menuAdapter = menuAdapter;
+        int count = menuAdapter.getCount();
+        for (int i = 0; i < count; i++) {
+//            获取tab
+            View tabView = menuAdapter.getTabView(i, mMenuTabView);
+            mMenuTabView.addView(tabView);
+            LinearLayout.LayoutParams params = (LayoutParams) tabView.getLayoutParams();
+            params.weight =1;
+            tabView.setLayoutParams(params);
+//            菜单的内容
+            View menuView = menuAdapter.getMenuView(i, mMenuContainerView);
+            menuView.setVisibility(GONE);
+            mMenuContainerView.addView(menuView);
+
+        }
     }
 }
